8e4c2acb632381774118a9e30017f6c9ac986b60
Lalu adryan <145007262+ydhstra@users.noreply.github.com>
2023-10-13 16:22:05 +0800

    Add files via upload

---
+++ tugas autentifiksi user.py
@@ -0,0 +1,10 @@
+
+username = input('masukan username :')
+password = input('masukan password :')
+code_username = 'lalu adryan'
+code_username = "12345"
+
+if username==code_username and password==code_password:
+    print("selamat datang",username)
+else:
+    print("username atau password salah")
